9c7b756ca254f9df0788716aef3085e67148bda7
Rashmi <mca.1962@unigoa.ac.in>
2020-12-16 15:11:47 +0530

    1962_paddle_movement,scorefile

--- 1962_paddle_movement,scorefile/paddle.java
+++ 1962_paddle_movement,scorefile/paddle.java
@@ -12,41 +12,45 @@ import javax.swing.JPanel;
 public class paddle extends JPanel implements KeyListener,ActionListener {
 
 	
-	private Boolean play = false;
+	private Boolean play = false;//because  game  shouldnt play by itself
 	private Timer timer;
-	private int delay = 3;
+	private int delay = 3; 
 	
-	private int playerX = 310;
+	private int playerX = 310; //starting position of paddle
 	
 	
 	
 	
 	public paddle() {
-		addKeyListener(this);
-		setFocusable(true);
-		setFocusTraversalKeysEnabled(true);
-		timer = new Timer(delay, this);
+		addKeyListener(this);   //added to work with keylistener 
+		setFocusable(true);    //Events are only dispatched to the component that has focus. 
+		//So your KeyEvent will only be dispatched to the panel if it is "focusable" and it has focus.
+		setFocusTraversalKeysEnabled(true); //allows to switch from one component to next upon pressing the tab key
+		timer = new Timer(delay, this); // creating timer object
 		timer.start();
 	}
 	
-public void paint(Graphics g) {
-		//paddle
+public void paint(Graphics g) { //graphics for creating diff shapes for bal,paddle and bricks
+		
+	//background
 	g.setColor(Color.black);
-	g.fillRect(1, 1, 692, 592);
+	g.fillRect(1, 1, 692, 592);//rectangle for background
+	
+	//border
 	g.setColor(Color.red);
 	g.fillRect(0, 0, 3, 592);     //border rectangle
 	g.fillRect(0, 0, 692, 3);		//border rectangle
 	g.fillRect(681, 0, 3, 592);
+	
+	//paddle
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(playerX, 550, 100, 8);   
-		g.setColor(Color.LIGHT_GRAY);
-		
 		g.dispose();
 	
 
 }
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e) { //to detect the arrow key for moving paddle
 		// TODO Auto-generated method stub
 		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			if(playerX >= 590) {
@@ -66,13 +70,11 @@ public void paint(Graphics g) {
 	}
 
 	private void moveLeft() {
-		// TODO Auto-generated method stub
 		play = true;
 		playerX-=15;
 	}
 
 	private void moveRight() {
-		// TODO Auto-generated method stub
 		play = true;
 		playerX+=15;
 		
@@ -93,13 +95,8 @@ public void paint(Graphics g) {
 	@Override
 	
 		public void actionPerformed(ActionEvent e) {
-
-			//ball-paddle intersection
-		//	if(new Rectangle(ballPosX, ballPosY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
-				//ballYdir = -ballYdir;
-			
-			
-			repaint();
+		timer.start();
+		repaint(); //its a function to recreate paddle
 	}
 		
 	}
